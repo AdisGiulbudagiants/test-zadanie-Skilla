61114d2181eed2585b4374dc7bfc5815e46c365f
Adis Giulbudagiants <adisforwork2001@gmail.com>
2023-06-22 18:40:37 +0400

    Sidebar is Finally done

---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+type Props = {
+  text: string
+  img?: any
+}
+
+const Button = (props: Props) => {
+  return (
+    <div className="flex justify-center w-[12.5rem] h-[3.25rem] pr-[1.25rem] bg-blue rounded-[0.25rem] hover:bg-opacity-[90%] hover:cursor-pointer active:scale-95 relative">
+      <div className=" flex justify-center items-center">
+        <p className="text-center font-Medium text-white text-[1rem] leading-[148%] select-none">
+          {props.text}
+        </p>
+      </div>
+      <div className=" absolute right-[1.25rem] mt-[0.875rem]">{props.img}</div>
+    </div>
+  )
+}
+
+export default Button
--- src/components/screens/sidebar/Sidebar.tsx
+++ src/components/screens/sidebar/Sidebar.tsx
@@ -1,8 +1,19 @@
 import { useState, useEffect } from "react"
-import Logo from "../../assets/Logo"
+import Logo from "../../assets/sidebar/Logo"
 import Section from "./Section"
-import SectionPhone from "../../assets/SectionPhone"
-import styles from "./Sidebar.module.css"
+import SectionPhone from "../../assets/sidebar/SectionPhone"
+import Baza from "../../assets/sidebar/Baza"
+import ContrAgents from "../../assets/sidebar/ContrAgents"
+import Docs from "../../assets/sidebar/Docs"
+import Ispolniteli from "../../assets/sidebar/Ispolniteli"
+import Itogi from "../../assets/sidebar/Itogi"
+import Message from "../../assets/sidebar/Message"
+import Orders from "../../assets/sidebar/Orders"
+import Otcheti from "../../assets/sidebar/Otcheti"
+import Settings from "../../assets/sidebar/Settings"
+import Plus from "../../assets/sidebar/Plus"
+import Exclamation from "../../assets/sidebar/Exclamation"
+import Button from "../../Button"
 
 const Sidebar = () => {
   const [activeNavLink, setActiveNavLink] = useState("")
@@ -16,7 +27,7 @@ const Sidebar = () => {
 
   useEffect(() => {
     const currentPath = window.location.pathname
-    const initialActiveSection = getInitialActiveSection(currentPath) // Здесь вам нужно определить, какой NavLink должен быть активным при загрузке страницы
+    const initialActiveSection = getInitialActiveSection(currentPath)
     setActiveNavLink(initialActiveSection)
   }, [])
 
@@ -24,81 +35,92 @@ const Sidebar = () => {
     setActiveNavLink(sectionName)
   }
   return (
-    <div className={styles.main}>
-      <div className="mb-[2rem]">
-        <Logo />
+    <>
+      <div className="w-[100%] h-[100%]  bg-darkBlue">
+        <div className="mb-[2rem]">
+          <Logo />
+        </div>
+        <Section
+          active={activeNavLink === "/1"}
+          setActive={() => handleNavLinkClick("/1")}
+          to="/1"
+          img={<Itogi />}
+          name={"Итоги"}
+        />
+        <Section
+          active={activeNavLink === "/2"}
+          setActive={() => handleNavLinkClick("/2")}
+          to="/2"
+          img={<Orders />}
+          name={"Заказы"}
+        />
+        <Section
+          active={activeNavLink === "/3"}
+          setActive={() => handleNavLinkClick("/3")}
+          to="/3"
+          img={<Message />}
+          name={"Сообщения"}
+        />
+        <Section
+          active={activeNavLink === "/"}
+          setActive={() => handleNavLinkClick("/")}
+          to="/"
+          img={<SectionPhone />}
+          name={"Звонки"}
+        />
+        <Section
+          active={activeNavLink === "/5"}
+          setActive={() => handleNavLinkClick("/5")}
+          to="/5"
+          img={<ContrAgents />}
+          name={"Контрагенты"}
+        />
+        <Section
+          active={activeNavLink === "/6"}
+          setActive={() => handleNavLinkClick("/6")}
+          to="/6"
+          img={<Docs />}
+          name={"Документы"}
+        />
+        <Section
+          active={activeNavLink === "/7"}
+          setActive={() => handleNavLinkClick("/7")}
+          to="/7"
+          img={<Ispolniteli />}
+          name={"Исполнители"}
+        />
+        <Section
+          active={activeNavLink === "/8"}
+          setActive={() => handleNavLinkClick("/8")}
+          to="/8"
+          img={<Otcheti />}
+          name={"Отчеты"}
+        />
+        <Section
+          active={activeNavLink === "/9"}
+          setActive={() => handleNavLinkClick("/9")}
+          to="/9"
+          img={<Baza />}
+          name={"База знаний"}
+        />
+        <Section
+          active={activeNavLink === "/10"}
+          setActive={() => handleNavLinkClick("/10")}
+          to="/10"
+          img={<Settings />}
+          name={"Настройки"}
+        />
+        {/**Функционала на кнопках здесь нет*/}
+        <div className="mt-[4rem]">
+          <div className="flex justify-center">
+            <Button img={<Plus />} text={"Добавить заказ"} />
+          </div>
+          <div className="mt-[2rem] flex justify-center">
+            <Button img={<Exclamation />} text={"Оплата"} />
+          </div>
+        </div>
       </div>
-      <Section
-        active={activeNavLink === "/1"}
-        setActive={() => handleNavLinkClick("/1")}
-        to="/1"
-        img={<SectionPhone />}
-        name={"Итоги"}
-      />
-      <Section
-        active={activeNavLink === "/2"}
-        setActive={() => handleNavLinkClick("/2")}
-        to="/2"
-        img={<SectionPhone />}
-        name={"Заказы"}
-      />
-      <Section
-        active={activeNavLink === "/3"}
-        setActive={() => handleNavLinkClick("/3")}
-        to="/3"
-        img={<SectionPhone />}
-        name={"Сообщения"}
-      />
-      <Section
-        active={activeNavLink === "/"}
-        setActive={() => handleNavLinkClick("/")}
-        to="/"
-        img={<SectionPhone />}
-        name={"Звонки"}
-      />
-      <Section
-        active={activeNavLink === "/5"}
-        setActive={() => handleNavLinkClick("/5")}
-        to="/5"
-        img={<SectionPhone />}
-        name={"Контрагенты"}
-      />
-      <Section
-        active={activeNavLink === "/6"}
-        setActive={() => handleNavLinkClick("/6")}
-        to="/6"
-        img={<SectionPhone />}
-        name={"Документы"}
-      />
-      <Section
-        active={activeNavLink === "/7"}
-        setActive={() => handleNavLinkClick("/7")}
-        to="/7"
-        img={<SectionPhone />}
-        name={"Исполнители"}
-      />
-      <Section
-        active={activeNavLink === "/8"}
-        setActive={() => handleNavLinkClick("/8")}
-        to="/8"
-        img={<SectionPhone />}
-        name={"Отчеты"}
-      />
-      <Section
-        active={activeNavLink === "/9"}
-        setActive={() => handleNavLinkClick("/9")}
-        to="/9"
-        img={<SectionPhone />}
-        name={"База знаний"}
-      />
-      <Section
-        active={activeNavLink === "/10"}
-        setActive={() => handleNavLinkClick("/10")}
-        to="/10"
-        img={<SectionPhone />}
-        name={"Настройки"}
-      />
-    </div>
+    </>
   )
 }
 
